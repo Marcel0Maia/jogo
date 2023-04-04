bc07d0f7de5fb79f7cf1b36065fe2eac9f1b1fbe
Marcel0Maia <marcelomaia.castro@gmail.com>
2023-04-04 16:39:16 -0300

    tab

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ function iniciar(){
 var myGameArea = {
 canvas : document.createElement("canvas"),
 start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
+    this.canvas.width = 500;
+    this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 }
 }
+
+    function peça(){
+        this.x = x;
+        this.y = y;
+    }
