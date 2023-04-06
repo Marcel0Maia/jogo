78bdb936460dd6e807edbca2400b402c8fc53523
Marcel0Maia <marcelomaia.castro@gmail.com>
2023-04-06 00:14:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,31 +51,31 @@ var linha = [
 
 function iniciar(){
     myGameArea.start();
-    whitePiece[0] = new piece(coluna[0], linha[0], 0, 0, "white", 3);
-    whitePiece[1] = new piece(coluna[2], linha[0], 0, 0, "white", 3);
-    whitePiece[2] = new piece(coluna[4], linha[0], 0, 0, "white", 3);
-    whitePiece[3] = new piece(coluna[6], linha[0], 0, 0, "white", 3);
-    whitePiece[4] = new piece(coluna[1], linha[1], 0, 0, "white", 3);
-    whitePiece[5] = new piece(coluna[3], linha[1], 0, 0, "white", 3);
-    whitePiece[6] = new piece(coluna[5], linha[1], 0, 0, "white", 3);
-    whitePiece[7] = new piece(coluna[7], linha[1], 0, 0, "white", 3);
-    whitePiece[8] = new piece(coluna[0], linha[2], 0, 0, "white", 3);
-    whitePiece[9] = new piece(coluna[2], linha[2], 0, 0, "white", 3);
-    whitePiece[10] = new piece(coluna[4], linha[2], 0, 0, "white", 3);
-    whitePiece[11] = new piece(coluna[6], linha[2], 0, 0, "white", 3);
+    blackPiece[0] = new piece(coluna[1], linha[0], 0, 0, "black", 3);
+    blackPiece[1] = new piece(coluna[3], linha[0], 0, 0, "black", 3);
+    blackPiece[2] = new piece(coluna[5], linha[0], 0, 0, "black", 3);
+    blackPiece[3] = new piece(coluna[7], linha[0], 0, 0, "black", 3);
+    blackPiece[4] = new piece(coluna[0], linha[1], 0, 0, "black", 3);
+    blackPiece[5] = new piece(coluna[2], linha[1], 0, 0, "black", 3);
+    blackPiece[6] = new piece(coluna[4], linha[1], 0, 0, "black", 3);
+    blackPiece[7] = new piece(coluna[6], linha[1], 0, 0, "black", 3);
+    blackPiece[8] = new piece(coluna[1], linha[2], 0, 0, "black", 3);
+    blackPiece[9] = new piece(coluna[3], linha[2], 0, 0, "black", 3);
+    blackPiece[10] = new piece(coluna[5], linha[2], 0, 0, "black", 3);
+    blackPiece[11] = new piece(coluna[7], linha[2], 0, 0, "black", 3);
 
-    blackPiece[0] = new piece(coluna[0], linha[5], 0, 0, "black", 3);
-    blackPiece[1] = new piece(coluna[2], linha[5], 0, 0, "black", 3);
-    blackPiece[2] = new piece(coluna[4], linha[5], 0, 0, "black", 3);
-    blackPiece[3] = new piece(coluna[6], linha[5], 0, 0, "black", 3);
-    blackPiece[4] = new piece(coluna[1], linha[6], 0, 0, "black", 3);
-    blackPiece[5] = new piece(coluna[3], linha[6], 0, 0, "black", 3);
-    blackPiece[6] = new piece(coluna[5], linha[6], 0, 0, "black", 3);
-    blackPiece[7] = new piece(coluna[7], linha[6], 0, 0, "black", 3);
-    blackPiece[8] = new piece(coluna[0], linha[7], 0, 0, "black", 3);
-    blackPiece[9] = new piece(coluna[2], linha[7], 0, 0, "black", 3);
-    blackPiece[10] = new piece(coluna[4], linha[7], 0, 0, "black", 3);
-    blackPiece[11] = new piece(coluna[6], linha[7], 0, 0, "black", 3);
+    whitePiece[0] = new piece(coluna[0], linha[5], 0, 0, "white", 3);
+    whitePiece[1] = new piece(coluna[2], linha[5], 0, 0, "white", 3);
+    whitePiece[2] = new piece(coluna[4], linha[5], 0, 0, "white", 3);
+    whitePiece[3] = new piece(coluna[6], linha[5], 0, 0, "white", 3);
+    whitePiece[4] = new piece(coluna[1], linha[6], 0, 0, "white", 3);
+    whitePiece[5] = new piece(coluna[3], linha[6], 0, 0, "white", 3);
+    whitePiece[6] = new piece(coluna[5], linha[6], 0, 0, "white", 3);
+    whitePiece[7] = new piece(coluna[7], linha[6], 0, 0, "white", 3);
+    whitePiece[8] = new piece(coluna[0], linha[7], 0, 0, "white", 3);
+    whitePiece[9] = new piece(coluna[2], linha[7], 0, 0, "white", 3);
+    whitePiece[10] = new piece(coluna[4], linha[7], 0, 0, "white", 3);
+    whitePiece[11] = new piece(coluna[6], linha[7], 0, 0, "white", 3);
 }
 
 var myGameArea = {
